4307871ef6b4ca680d079ec0c3697a6803272a3a
Pievisco89 <pie-89@live.it>
2021-04-26 17:48:45 +0200

    non prende parseInt

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 $(function(){
 
-  var livello = parseInt(prompt('Livello: con quanti numeri vuoi giocare?'));
-
   var arrNumComputer;
   var arrNumUtente = [];
   var arrRisultato = [];
 
   reset();
 
+  //click su start e scateno gli eventi
   $('#start').click(function(){
 
     $(this).hide();
-
-    arrNumComputer = arrNumGenerator(livello, 100);
+    //uso la funzione creata per generare numeri random e pusharli nell'arrNumComputer
+    arrNumComputer = arrNumGenerator(5, 100);
     console.log(arrNumComputer);
-
+    //stampo arrNumComputer
     printOutput(arrNumComputer, '#display');
-
+    //vedo arrNumComputer per 5 secondi, alla fine deve apparire box
     setTimeout(function(){
       printOutput('Inserisci i numeri', '#display');
       $('#box').show();
@@ -24,16 +23,19 @@ $(function(){
     
   });
 
+  //click su invia
   $('#btn').click(function(){
-
+    //pusho valore input nell'arrNumUtente
     arrNumUtente.push($('input').val());
     console.log(arrNumUtente);
-    $('input').val('');
+    $('input').val(''); //svuoto sempre l'input ad ogni click su invia
 
+    //quando i due array hanno lenght uguale sparisce box con input e bottone
     if(arrNumComputer.length === arrNumUtente.length){
       $('#box').hide();
-      printOutput('Controllo in corso', '#display');
+      printOutput('Controllo in corso', '#display'); //stampo controllo in corso
 
+      //dopo 3 secondi di controllo o stampo arrRisultato o stampo sconfitta utente
       setTimeout(function(){
 
         var numeroPresente = false;
@@ -56,6 +58,7 @@ $(function(){
           
         };
        
+        //ricompare bottone start con scritta ricomincia
         $('#start').text('Ricomincia').show();
         
       }, 3000);
@@ -86,10 +89,10 @@ function arrNumGenerator(num, max){
     if(arrNum.includes(numRandom) === false){    
       arrNum.push(numRandom); 
     }
-  }
+  };
   return arrNum;
-}
+};
 
 function getNumRandom(max){
   return Math.floor(Math.random() * max) + 1;
-}
+};
